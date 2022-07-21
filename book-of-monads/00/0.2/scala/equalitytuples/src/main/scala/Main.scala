afdcfca7ba1a27ffdf0a7b7725a2b233308b4304
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-07-21 15:39:19 +0200

    scala scaffold

--- book-of-monads/00/0.2/scala/equalitytuples/src/main/scala/Main.scala
+++ book-of-monads/00/0.2/scala/equalitytuples/src/main/scala/Main.scala
@@ -1,5 +1,7 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+def tuplesEqual[A, B](t1: (A, B), t2: (A, B)) : Boolean =
+  val (t1a, t1b) = t1
+  val (t2a, t2b) = t2
+  t1a == t2a && t1b == t2b
 
-def msg = "I was compiled by Scala 3. :)"
+@main def hello(): Unit =
+  println("I compare tuples in a language that has tuple equality already")
