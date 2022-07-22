e49214e33cb549eb3882f38448e5671eb2b0afe8
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-07-22 15:35:21 +0200

    cleanup

--- book-of-monads/00/0.2/haskell/equality.hs
+++ book-of-monads/00/0.2/haskell/equality.hs
@@ -1,14 +1,30 @@
-data MyOwnTuple a b = MyOwnTuple a b
+import Text.Printf
+class MyEq a where
+  (===) :: a -> a -> Bool
 
-instance (Eq a, Eq b) => Eq (MyOwnTuple a b) where
-  (==) (MyOwnTuple a b) (MyOwnTuple a' b') = a == a' && b == b'
+instance (Eq a, Eq b) => MyEq (a, b) where
+  (===) (a, b) (a', b') = a == a' && b == b'
 
 
-oneTuple = MyOwnTuple 1 3
-sameTuple = MyOwnTuple 1 3
-differentTuple = MyOwnTuple 1 2
+oneTuple = (1, 3)
+sameTuple = (1, 3)
+differentTuple = (1, 2)
 
-main =
-  if oneTuple == sameTuple
-  then putStrLn "Equal"
-  else putStrLn "Different"
+renderTuple :: (Int, Int) -> String
+renderTuple (a, b) =
+  printf "( %d %d )" a b
+
+
+compareEqual =
+  putStrLn ("Comparing " ++ renderTuple oneTuple ++ " " ++ renderTuple sameTuple)
+  >>  if oneTuple === sameTuple
+      then putStrLn "Equal"
+      else putStrLn "Different"
+
+compareDifferent =
+  putStrLn ("Comparing " ++ renderTuple oneTuple ++ " " ++ renderTuple differentTuple)
+  >>  if oneTuple === differentTuple
+      then putStrLn "Equal"
+      else putStrLn "Different"
+
+main = compareEqual >> compareDifferent
