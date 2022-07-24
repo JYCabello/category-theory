006e66e0f280c9f1cf9265ff42b4aa200158aebf
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-07-24 20:24:13 +0200

    Experimenting with string formatting

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # category-theory
+
+Scala project creation: `sbt new scala/scala3.g8`
--- book-of-monads/00/0.2/haskell/equality.hs
+++ book-of-monads/00/0.2/haskell/equality.hs
@@ -6,24 +6,20 @@ instance (Eq a, Eq b) => MyEq (a, b) where
   (===) (a, b) (a', b') = a == a' && b == b'
 
 
+oneTuple :: (Int, Int)
 oneTuple = (1, 3)
 sameTuple = (1, 3)
 differentTuple = (1, 2)
 
-renderTuple :: (Int, Int) -> String
-renderTuple (a, b) =
-  printf "( %d %d )" a b
-
-
 compareEqual =
-  putStrLn ("Comparing " ++ renderTuple oneTuple ++ " " ++ renderTuple sameTuple)
+  putStrLn (printf "Comparing %s %s" (show oneTuple) (show sameTuple))
   >>
     if oneTuple === sameTuple
     then putStrLn "Equal"
     else putStrLn "Different"
 
 compareDifferent =
-  putStrLn ("Comparing " ++ renderTuple oneTuple ++ " " ++ renderTuple differentTuple)
+  putStrLn (printf "Comparing %v %s" (show oneTuple) (show differentTuple))
   >>
     if oneTuple === differentTuple
     then putStrLn "Equal"
