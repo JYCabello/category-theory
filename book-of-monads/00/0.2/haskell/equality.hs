20a615604bcb84952e41c5e20a02709f5e252133
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-07-24 21:12:42 +0200

    Overcomplicated the whole thing.

--- book-of-monads/00/0.2/haskell/equality.hs
+++ book-of-monads/00/0.2/haskell/equality.hs
@@ -5,24 +5,17 @@ class MyEq a where
 instance (Eq a, Eq b) => MyEq (a, b) where
   (===) (a, b) (a', b') = a == a' && b == b'
 
+instance (Show a, Show b) => PrintfArg (a, b) where
+  formatArg (a, b) f str = formatArg (show (a, b)) f str
 
 oneTuple :: (Int, Int)
 oneTuple = (1, 3)
 sameTuple = (1, 3)
 differentTuple = (1, 2)
+compareWith :: (Int, Int) -> IO()
+compareWith other =
+  let result = if oneTuple === other then "equal" else "different" 
+  in
+    putStrLn (printf "Comparing %s and %s and they are %s" oneTuple other result)
 
-compareEqual =
-  putStrLn (printf "Comparing %s %s" (show oneTuple) (show sameTuple))
-  >>
-    if oneTuple === sameTuple
-    then putStrLn "Equal"
-    else putStrLn "Different"
-
-compareDifferent =
-  putStrLn (printf "Comparing %v %s" (show oneTuple) (show differentTuple))
-  >>
-    if oneTuple === differentTuple
-    then putStrLn "Equal"
-    else putStrLn "Different"
-
-main = compareEqual >> compareDifferent
+main = compareWith sameTuple >> compareWith differentTuple
