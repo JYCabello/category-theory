d667961f29c3e9805adee629e796575e65b785e2
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-07-24 21:19:40 +0200

    Removed unneeded type annotations

--- book-of-monads/00/0.2/haskell/equality.hs
+++ book-of-monads/00/0.2/haskell/equality.hs
@@ -8,14 +8,13 @@ instance (Eq a, Eq b) => MyEq (a, b) where
 instance (Show a, Show b) => PrintfArg (a, b) where
   formatArg (a, b) f str = formatArg (show (a, b)) f str
 
-oneTuple :: (Int, Int)
 oneTuple = (1, 3)
 sameTuple = (1, 3)
 differentTuple = (1, 2)
-compareWith :: (Int, Int) -> IO()
+
 compareWith other =
-  let result = if oneTuple === other then "equal" else "different" 
-  in
-    putStrLn (printf "Comparing %s and %s and they are %s" oneTuple other result)
+  let result = if oneTuple === other then "equal" else "different" in
+  let text = printf "Comparing %s and %s and they are %s" oneTuple other result in
+  putStrLn text
 
 main = compareWith sameTuple >> compareWith differentTuple
