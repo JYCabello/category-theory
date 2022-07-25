5faeee970f0bf14bc7d52bebc28c6e4bd01f94cf
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-07-25 15:36:08 +0200

    First excercise scala with cats

--- scalawithcats/01/1.3/printable/src/main/scala/Main.scala
+++ scalawithcats/01/1.3/printable/src/main/scala/Main.scala
@@ -1,7 +1,34 @@
+final case class Cat(name: String, age: Int, color: String)
 
+trait Printable[A] {
+  def format(a: A) : String
+}
+import PrintableSyntax._
+implicit object PrintableInstances {
+  implicit val printableString: Printable[String] = (a: String) => a
+  implicit val printableInt: Printable[Int] = (a: Int) => a.toString
+  implicit val printableCat: Printable[Cat] =
+    (c: Cat) => s"${c.name.format} is a ${c.age.format} year-old ${c.color.format} cat."
+}
+
+object Printable {
+  def format[A](a: A)(implicit p: Printable[A]): String = p.format(a)
+  def print[A](a: A)(implicit p: Printable[A]): Unit = println(p.format(a))
+}
+
+object PrintableSyntax {
+  implicit class PrintableOps[A](value: A) {
+    def format(implicit w: Printable[A]): String = Printable.format(value)
+    def print(implicit w: Printable[A]): Unit = Printable.print(value)
+  }
+}
+
+import PrintableInstances._
 
 @main def hello(): Unit =
   println("Hello world!")
   println(msg)
+  7.print
+  Cat("Garfield", 5, "orange").print
 
 def msg = "I was compiled by Scala 3. :)"
